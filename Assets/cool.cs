603864b658b36e1ea0072614479c4cbf06b549ff
Ian Justiz <justiz.ian8@gmail.com>
2023-05-30 23:11:10 -0400

    Initial commit

---
+++ Assets/cool.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        throw new Exception();
+    }
+
+
+// Update is called once per frame
+    void Update()
+    {
+        throw new Exception();
+    }
+}
